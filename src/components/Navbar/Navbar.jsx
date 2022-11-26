c7be50b8221e8e96ab455c43b7a22f462cb7ffa2
rishabhkumar <1741rishabh@gmail.com>
2022-11-27 01:31:14 +0530

    Navbar Component ready

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import logo from '../../assets/img/vit-logo-1.png'
+import './Navbar.css'
+function Navbar()
+{
+    return(
+    <nav className="nav">
+        <img src={logo} alt="img" />
+        <h1>VIT BHOPAL</h1>
+        <div className='nav_buttons'><button className="log_in_button">Login</button> <button className="register_button">Register</button></div>
+
+    </nav>)
+}
+
+export default Navbar;
+
